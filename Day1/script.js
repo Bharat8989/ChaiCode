62067ea3cfb4d4297f755323796247766899a430
Bharat <kadamb208@gmail.com>
2024-07-14 20:32:04 +0530

    initial commit

---
+++ Day1/script.js
@@ -0,0 +1,31 @@
+//variable declarations
+var name='Bharat Kadam'
+console.log(name)
+const lastName='kadam'
+console.log(lastName)
+let age=23;
+console.log(age)
+//reassigning variables
+
+let initialVal=23;
+let newVal=34;
+console.log(initialVal)
+
+initialVal=33;
+console.log(initialVal)
+
+//datatypes 
+
+let num=23;
+console.log(typeof(num))
+let name1='Bharat'
+console.log(typeof(name1))
+console.log(typeof(true))
+const object={
+    firstName:'Bharat',
+    lastName:'Kadam',
+    age:43
+}
+console.log(typeof(object))
+var arr=[2,34,3,2,34]
+console.log(typeof(arr))
