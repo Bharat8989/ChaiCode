7a7915560ea1cc58ca45a3085c10e68b17f72753
Bharat <kadamb208@gmail.com>
2024-07-24 21:42:36 +0530

    adding day 7

---
+++ Day7/script.js
@@ -0,0 +1,15 @@
+ //array creation and access
+
+ //create and array of numbers form 1 to 5 
+
+ const studentName=[
+    'Bharat',
+    'Suraj',
+    'Pavan',
+
+ ]
+ console.log(studentName)
+
+ console.log("print the two number")
+
+ 
