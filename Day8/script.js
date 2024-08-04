6a465218162ed1331225ca76ec8dc64adf909292
Bharat <kadamb208@gmail.com>
2024-08-04 20:07:50 +0530

    adding the ES6

--- Day8/script.js
+++ Day8/script.js
@@ -1,3 +1,8 @@
-console.log(
-    "hello pinki"
-)
+//ES6 features
+// template literals 
+//use template literals to create a string that includes variables for a person's name and age and log the string to the console
+const name = "Alice";
+const age = 30;
+
+const message = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message);
