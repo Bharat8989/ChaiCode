88fdb8e7b967cfa44ed9cdb815b98e3ed6252d4e
Bharat <kadamb208@gmail.com>
2024-08-05 08:01:54 +0530

    day 9 add

--- Day9/script.js
+++ Day9/script.js
@@ -1,1 +1,13 @@
-console.log("J")
+// //select an html element by id and change its text content
+let demoElement = document.getElementById("demo");
+        demoElement.innerHTML = "Hello, world!";
+        console.log(demoElement);
+
+//class
+
+// const backgroundColorElements = document.getElementsByClassName("color");
+
+for (let i = 0; i < backgroundColorElements.length; i++) {
+    backgroundColorElements[i].innerHTML = "Background color";
+    backgroundColorElements[i].style.backgroundColor = "red";
+}
