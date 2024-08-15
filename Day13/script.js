6ea5912579762564bf601b6618ae7b7e7ec855ff
Bharat <kadamb208@gmail.com>
2024-08-15 12:06:49 +0530

    adding files day 13

---
+++ Day13/script.js
@@ -0,0 +1,10 @@
+//modules
+//creating and exporting modules
+
+import { multiply  } from "./utility.js";
+const result =multiply(1,2,4)
+console.log(result)
+
+import { addition } from "./utility.js";
+const result1= addition(2,3)
+console.log(result1)
---
+++ Day13/utility.js
@@ -0,0 +1,10 @@
+export function multiply(a,b,c)
+{
+    return a*b*c;
+}
+ function add(x,y){
+    return x+y;
+}
+export {
+    add as addition
+}
