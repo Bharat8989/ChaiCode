663104d862a941321a3d3308088051366c2caacb
Bharat <kadamb208@gmail.com>
2024-08-17 11:06:07 +0530

    adding files

---
+++ One-Short-JS1/main.js
@@ -0,0 +1,45 @@
+import './style.css';
+
+// This will add HTML content to the element with id 'app'
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+  </div>
+`;
+
+// Simple console logs
+let count1 = 12;
+console.log(count1);
+console.log("hello world");
+
+// console.log(b)
+let b=23;
+ console.log(b)
+
+
+ let c=[12,32];
+ let d=c
+ console.log(c)
+ console.log(d)
+ 
+ d.pop();
+ console.log(d)
+console.log(c)
+
+console.log("print the number of 1 to 10 using while loop")
+
+for(let i=1; i<=10;i++)
+  {
+    console.log(i)
+  }
+
+
+  console.log('functions')
+
+
+  function main(){
+    console.log("hello bolo")
+
+  }
+  main();
+  
